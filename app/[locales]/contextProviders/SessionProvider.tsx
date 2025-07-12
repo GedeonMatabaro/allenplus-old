14922f6c51ac40de64ac49310609d5eacf208169
GedeonMatabaro <mugishomatgedeon@gmail.com>
2025-07-12 08:05:46 -0400

    correcting contact and contex pages

--- app/[locales]/contextProviders/SessionProvider.tsx
+++ app/[locales]/contextProviders/SessionProvider.tsx
@@ -1,3 +1,34 @@
+// "use client";
+
+
+// import { UserData } from "@/lib/types";
+// import type { User, Session } from "@prisma/client";
+// import React, { createContext, useContext } from "react";
+
+
+// interface SessionContext {
+//   user: UserData|null;
+//   session: Session|null
+// }
+
+// const SessionContext = createContext<SessionContext | null>(null);
+
+// export default function SessionProvider({
+//   children,
+//   value,
+// }: React.PropsWithChildren<{ value: SessionContext }>) {
+//   return (
+//     <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
+//   );
+// }
+
+// export function useSession() {
+//   const context = useContext(SessionContext);
+//   if (!context) {
+//     throw new Error("useSession must be used within a SessionProvider");
+//   }
+//   return context;
+// }
 "use client";
 
 
