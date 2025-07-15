8a3d5bb5e0c0e3016c1ee20096271a8b8eff896b
GedeonMatabaro <mugishomatgedeon@gmail.com>
2025-07-15 19:43:40 -0400

    changing layout - cleaning

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,25 +1,4 @@
-// import { notFound, redirect } from "next/navigation";
 
-// import { NextIntlClientProvider, hasLocale } from "next-intl";
-// import { routing } from "@/i18n/routing";
-
-// export default async function Layout({
-//   children,
-//   params,
-// }: Readonly<{
-//   children: React.ReactNode;
-//   params: Promise<{ locale: string }>;
-// }>) {
-//   const { locale } = await params;
-//     if (!hasLocale(routing.locales, locale)) {
-//       notFound();
-//     }
-//   return <>
-//       <NextIntlClientProvider>
-//         {children}
-//       </NextIntlClientProvider>
-//     </>
-// }
 import { notFound, redirect } from "next/navigation";
 
 import { NextIntlClientProvider, hasLocale } from "next-intl";
